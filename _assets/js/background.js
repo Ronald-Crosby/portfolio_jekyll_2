eae7b21103572bcc3d67460e8034197284077da5
Olly Robinson <ollythedeveloper@gmail.com>
2020-04-28 17:24:55 +0100

    update background code use Intersection Observer

--- _assets/js/background.js
+++ _assets/js/background.js
@@ -2,24 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
   const backgroundTag = document.getElementById('background')
   const colorSections = document.querySelectorAll('[data-bg-color]')
 
-  let colorSectionData = []
+    const myObserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0.1) {
+          const bgColor = entry.target.getAttribute('data-bg-color')
+          backgroundTag.className = bgColor
+        }
+      })
+    }, {
+      threshold: [0.1, 0.9]
+    })
+  
 
   colorSections.forEach(section => {
-    colorSectionData.push({
-      offset: parseInt(section.getBoundingClientRect().top + window.pageYOffset),
-      classes: section.getAttribute('data-bg-color'),
-    })
+    myObserver.observe(section)
   })
-
-  const updateBackground = () => {
-    const windowScroll = window.pageYOffset
-    colorSectionData.forEach(({ offset, classes }) => {
-      if (windowScroll + 300 >= offset) {
-        backgroundTag.className = classes
-      }
-    })
-  }
-
-  document.addEventListener('scroll', updateBackground)
-  updateBackground()
 })
